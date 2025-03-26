06ae7f4b64d74bdf035a72662fac773d972d7251
harshit nagar <nharshit7024@gmail.com>
2025-03-26 09:57:38 +0530

    created react setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+       <div className='text-4xl'>
+hello chak de pdf
+       </div>
+    </>
+  )
+}
+
+export default App
